6c9b320d4148906f79b52dab84420c257e18571f
maallikarjun <azureuser@TestVMArjun.xm3ineb2swiehelno2dzhrzkch.bx.internal.cloudapp.net>
2021-08-11 12:09:46 +0000

    vote

---
+++ src/main/java/servlet/voteServlet.java
@@ -0,0 +1,47 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import pojo.Vote;
+
+/**
+ * Servlet implementation class voteServlet
+ */
+public class voteServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+		protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			// TODO Auto-generated method stub
+			HttpSession session=request.getSession();
+			PrintWriter out=response.getWriter();
+			String Name=request.getParameter("name");
+			int Age=Integer.parseInt(request.getParameter("age"));
+			session.setAttribute("name",Name);
+			session.setAttribute("age", Age);
+			if (Age<=17) {
+				out.println("<html>"
+				+"<body>"
+				+"<h1 style=\"background-color: red; color:yellow;\">Hi "+Name+" <br>You are not Eligible for vote</h1>"
+				+"</body>"
+				+"</html>");
+			} else {
+				out.println("<html>"
+						+"<body>"
+						+"<h1 style=\"background-color: green; color:yellow;\">Hi "+Name+" <br>You are Eligible for vote</h1>"
+						+"</body>"
+						+"</html>");
+			}
+			
+			
+		}
+
+	}
+
+
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,15 +1,17 @@
-<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1" import="java.util.Scanner"%>
-
 <html>
+<title>Vote</title>
 <body>
-<h1>Hello World!
-
-	<br>	TestDemo1 <br>
-
-Hello Hcl How are you.
-</h1>
-
+<h2>Hello World!</h2>
+<form action="index" method="post">
+<table><tr><th>
+<label>Name</label></th><td>
+<input type="text" name="name"></td></tr>
+<tr><th>
+<label>Age</label></th><td>
+<input type="number" name="age"></td></tr>
+<tr><td></td><td><input type="submit" value="Submit"></td></tr>
+</table>
+</form>
 </body>
 </html>
 
