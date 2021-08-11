dba287709b53bd2dbdbd48b93349e5138d805542
Maallikarjun <87408544+Maallikarjun@users.noreply.github.com>
2021-08-11 17:49:27 +0530

    Update voteServlet.java

--- src/main/java/servlet/voteServlet.java
+++ src/main/java/servlet/voteServlet.java
@@ -1,22 +1,13 @@
 package servlet;
-
 import java.io.IOException;
 import java.io.PrintWriter;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
-import pojo.Vote;
-
-/**
- * Servlet implementation class voteServlet
- */
 public class voteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
 		protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 			// TODO Auto-generated method stub
 			HttpSession session=request.getSession();
@@ -38,10 +29,5 @@ public class voteServlet extends HttpServlet {
 						+"</body>"
 						+"</html>");
 			}
-			
-			
 		}
-
-	}
-
-
+}
