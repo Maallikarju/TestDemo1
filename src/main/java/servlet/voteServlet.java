9763144938a73c8d4a7af746a08cd44e25b0ce42
Maallikarjun <87408544+Maallikarjun@users.noreply.github.com>
2021-08-12 09:52:45 +0530

    Update voteServlet.java

--- src/main/java/servlet/voteServlet.java
+++ src/main/java/servlet/voteServlet.java
@@ -8,12 +8,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
-import pojo.Vote;
-
-/**
- * Servlet implementation class voteServlet
- */
 public class voteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
