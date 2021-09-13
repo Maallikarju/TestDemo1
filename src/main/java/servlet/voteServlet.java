a3d08d76a2f856de98934cc9d3c95d2d0c9517ee
mallikarjungit001 <89639222+mallikarjungit001@users.noreply.github.com>
2021-09-13 09:47:01 +0530

    Update voteServlet.java

--- src/main/java/servlet/voteServlet.java
+++ src/main/java/servlet/voteServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 public class voteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+                @Override
 		protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 			// TODO Auto-generated method stub
 			HttpSession session=request.getSession();
