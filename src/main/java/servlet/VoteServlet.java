65ebc594f071852cb89e150d297dad96f6ad1bbd
mallikarjungit001 <89639222+mallikarjungit001@users.noreply.github.com>
2021-09-20 13:17:19 +0530

    Update VoteServlet.java

--- src/main/java/servlet/VoteServlet.java
+++ src/main/java/servlet/VoteServlet.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import java.io.IOException;
+
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
@@ -8,9 +9,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-public class VoteServlet extends HttpServlet {
+public class voteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-                @Override
+       
 		protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 			// TODO Auto-generated method stub
 			HttpSession session=request.getSession();
@@ -18,24 +19,24 @@ public class VoteServlet extends HttpServlet {
 			String Name=request.getParameter("name");
 			int Age=Integer.parseInt(request.getParameter("age"));
 			session.setAttribute("name",Name);
-			session.setAttribute("age", Age);.
-			
-			if (Age<=17)
-			{
+			session.setAttribute("age", Age);
+			int ok=18-Age;
+			if (Age<=17) {
 				out.println("<html>"
 				+"<body><center>"
-				+"<h1 style=\"background-color: red; color:yellow;\">Hi "+Name+" <br>You are not Eligible for voting tell years</h1>"
+				+"<h1 style=\"background-color: red; color:yellow;\">Hi "+Name+" <br>You are not Eligible for voteing tell "+ok+" years</h1>"
 				+"</center></body>"
 				+"</html>");
 			} else {
 				out.println("<html>"
 						+"<body><center>"
-						+"<h1 style=\"background-color: green; color:yellow;\">Hi "+Name+" <br>You are Eligible for voting</h1>"
+						+"<h1 style=\"background-color: green; color:yellow;\">Hi "+Name+" <br>You are Eligible for voteing</h1>"
 						+"</center></body>"
 						+"</html>");
 			}
+			
+			
 		}
 
 	}
-
 
